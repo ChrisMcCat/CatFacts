49524319f3287bd29ce646a0061ea40a364e9bdf
ChrisMcCat <ichris.kk@gmail.com>
2020-01-28 09:45:55 +0100

    Added classes to support filling database with json form api

--- src/main/java/com/Chris/CatFacts/CatFactsApplication.java
+++ src/main/java/com/Chris/CatFacts/CatFactsApplication.java
@@ -4,6 +4,9 @@ import com.Chris.CatFacts.domain.Facts;
 import com.Chris.CatFacts.service.JsonService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,19 +23,23 @@ public class CatFactsApplication {
 		SpringApplication.run(CatFactsApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner runner(JsonService jsonService) {
-		return args -> {
-			ObjectMapper mapper = new ObjectMapper();
-			TypeReference<List<Facts>> typeReference = new TypeReference<List<Facts>>()();
-			InputStream inputStream = TypeReference.class.getResourceAsStream("apijson");
-			try {
-				List<Facts> facts = mapper.readValue(inputStream,typeReference);
-				jsonService.save(facts);
-				System.out.println("json saved");
-			} catch (IOException e) {
-				System.out.println("unable to save " + e.getMessage());
-			}
-		};
-	}
+//	@Bean
+//	CommandLineRunner runner(JsonService jsonService) throws UnirestException {
+//		HttpResponse<String> response = Unirest.get("https://brianiswu-cat-facts-v1.p.rapidapi.com/facts")
+//				.header("x-rapidapi-host", "brianiswu-cat-facts-v1.p.rapidapi.com")
+//				.header("x-rapidapi-key", "ab641fc238msh8b82c9ee652a6c2p10f75fjsn4b75aad01ba7")
+//				.asString();
+//		return args -> {
+//			ObjectMapper mapper = new ObjectMapper();
+//			TypeReference<List<Facts>> typeReference = new TypeReference<List<Facts>>(){};
+//			InputStream inputStream = TypeReference.class.getResourceAsStream(String.valueOf(response));
+//			try {
+//				List<Facts> facts = mapper.readValue(inputStream,typeReference);
+//				jsonService.save(facts);
+//				System.out.println("json saved");
+//			} catch (IOException e) {
+//				System.out.println("unable to save " + e.getMessage());
+//			}
+//		};
+//	}
 }
